14b47f019d40af44941b47fa8fd2f180ddf57307
nicocasaisd <nicolas.casais.dassie@gmail.com>
2023-04-28 11:50:33 -0300

    ej09

--- clase01/ej09.php
+++ clase01/ej09.php
@@ -9,16 +9,32 @@ lapicera
 echo '<h1>Ejercicio 09 y 10</h1>';
 
 // Creamos el array asociativo
-$lapicera1 = array('color'=>'azul', 'marca'=>'bic', 'trazo'=>'fino', 'precio'=>100);
-$lapicera2 = array('color'=>'rojo', 'marca'=>'faber', 'trazo'=>'grueso', 'precio'=>80);
-$lapicera3 = array('color'=>'verde', 'marca'=>'maped', 'trazo'=>'semi-grueso', 'precio'=>150);
+$lapicera1 = array('color' => 'azul', 'marca' => 'bic', 'trazo' => 'fino', 'precio' => 100);
+$lapicera2 = array('color' => 'rojo', 'marca' => 'faber', 'trazo' => 'grueso', 'precio' => 80);
+$lapicera3 = array('color' => 'verde', 'marca' => 'maped', 'trazo' => 'semi-grueso', 'precio' => 150);
 
-// Creamos el array de arrays
+// Creamos el array de arrays (indexado)
 $arr_de_lapiceras = array($lapicera1, $lapicera2, $lapicera3);
 
-foreach($arr_de_lapiceras as $lapicera){
+// Creamos el array de arrays (asociativo)
+$lapiceras_asociativo['L1'] = $lapicera1;
+$lapiceras_asociativo['L2'] = $lapicera2;
+$lapiceras_asociativo['L3'] = $lapicera3;
+
+// Mostramos
+
+echo '<h1>Array asociativo <h1/>';
+foreach ($lapiceras_asociativo as $key => $lapicera) {
+    echo '<h3> Lapicera </h3>';
+    foreach($lapicera as $key => $value){
+        echo $key,': ', $value, '<br/>';
+    }
+
+}
+echo '<h1>Array indexadao <h1/>';
+foreach ($arr_de_lapiceras as $lapicera) {
     echo '<h3> Lapicera </h3>';
     foreach ($lapicera as $key => $value) {
-        echo 'Valor del elemento ', $key, ': ', $value, '<br/>';
+        echo $key,': ', $value, '<br/>';
     }
 }
